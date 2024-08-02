7cec584787e18809c92ecc2f819e84b56fed0170
awaludin-dev <ubedsf@gmail.com>
2024-08-02 15:20:52 +0800

    message after send the form

--- js/to-spreadsheet.js
+++ js/to-spreadsheet.js
@@ -1,6 +1,7 @@
 const form = document.forms['form-submit']
 const submitButton = document.querySelector("#submitButton")
-const scriptURL = 'https://script.google.com/macros/s/AKfycbx6n97guRSi8aE2HljAicJf9pt5tVCdj4UMHVwV0K0q4j_xP82HKkfc4v_mr8rKERQI/exec'
+const scriptURL = 'https://script.google.com/macros/s/AKfycbzqh_4uTw0JJat7kSzjLubJnMCvQzM-uvSx98Qq19BbhyVEenVHyEEx6HGV8Quu13dB/exec'
+const closeForm = document.querySelector("#floatingComponent")
 
 form.addEventListener('submit', e => {
     submitButton.disabled = true
@@ -8,13 +9,20 @@ form.addEventListener('submit', e => {
     let requestBody = new FormData(form)
     fetch(scriptURL, { method: 'POST', body: requestBody, mode: 'cors'})
     .then(response => {
-        console.log(response);
-        alert('Success!', response)
+        closeForm.setAttribute("style", "display: block")
         submitButton.disabled = false 
     })
     .catch(error => {
-        alert('Error', error.message)
         submitButton.disabled = false
     })
     form.reset()
 })
+
+function toggleComponent() {
+    const component = document.getElementById('floatingComponent');
+    if (component.style.display === 'none' || component.style.display === '') {
+        component.style.display = 'block'; // Show the component
+    } else {
+        component.style.display = 'none'; // Hide the component
+    }
+}
